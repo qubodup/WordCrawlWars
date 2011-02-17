0ad597c513a64d4bf53713be2157b4f92c17b53c
Iwan Gabovitch <qubodup@gmail.com>
2011-02-17 09:31:12 +0100

    new block structure and working movement and controls

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ Blocks.__index = Blocks
 function Blocks.create()
 	local blcks = {}
 	setmetatable(blcks,Blocks)
-	-- the actual blocks.. in the blocks.. in the blocks..? it's a table of x tables of y booleans
+	-- the actual blocks.. in the block cells.. in the block lines.. it's a table of x tables of y booleans
 	blcks.blocks = {}
 	-- top position (top is first line)
 	blcks.linepos = 0
@@ -20,10 +20,22 @@ function Blocks:move()
 	self.linepos = self.linepos - 1
 end
 
+function Blocks:sidestep(direction)
+	if direction == "left" then
+		-- I don't understand why I have to add one
+		table.insert(self.blocks[1],#self.blocks[1]+1,self.blocks[1][1])
+		table.remove(self.blocks[1],1)
+	elseif direction == "right" then
+		table.insert(self.blocks[1],1,self.blocks[1][#self.blocks[1]])
+		table.remove(self.blocks[1],#self.blocks[1])
+	end
+end
+
 function Blocks:collide(otherblocks)
 	local collisionExists = false
-	for i,v in ipairs(self.blocks) do
-		if true then collisionExists = true end
+	for i,line in ipairs(self.blocks) do
+		-- line = 1 handled elsewhere
+		if i ~= 1 and otherblocks then collisionExists = true end
 	end
 	return collisionExists
 end
@@ -40,8 +52,8 @@ function love.load()
 		-- the playfield is called map here (sorry for being an rts player)
 		-- limited by screen size and padding of one block
 	base.map = {
-		columns = math.floor(love.graphics.getWidth() / base.block.size) - 2 * base.block.size,
-		rows = math.floor(love.graphics.getHeight() / base.block.size) - 2 * base.block.size,
+		maxwidth = math.floor(love.graphics.getWidth() / base.block.size) - base.block.size,
+		maxheight = math.floor(love.graphics.getHeight() / base.block.size) - 2 * base.block.size,
 	}
 	-- title means menu or something like that
 	stage = "title"
@@ -54,22 +66,24 @@ function love.load()
 	timer = {
 		fall = {
 			counter = 0,
-			limit = 0.8, -- initial game speed so to say
+			limit = 0.7, -- initial game speed so to say
 		},
 	}
 	-- attacker, fixed and falling blocks blocks (ugh..)
 	blocksAttacker = Blocks.create()
-	blocksAttacker.linepos = base.map.rows + 7
-	blocksFixed = Blocks.create()
+	blocksAttacker.linepos = base.map.maxheight + 7
 	blocksFalling = Blocks.create()
+	-- align middle title text
+	blocksFalling.linepos = math.floor(base.map.maxheight / 2)
+	blocksFixed = Blocks.create()
 end
 
 function startWar(textfile)
 	blocksAttacker.blocks = text2blocks(file2text(textfile))
+	blocksFalling.blocks = text2blocks(file2text("title.txt"))
 	stage = "game"
 	timer.fall.counter = 0
 end
-
 
 -- file goes in, table of lines (text) goes out
 function file2text(filename)
@@ -82,28 +96,62 @@ end
 
 -- table of lines goes in, table of blocks goes out
 function text2blocks(text)
-	local blocks = {}
-	-- the following two vars are for letter wrap and count blocks, not pixels
-	local lineBlocks = 0
-	local currLine = 1
+	local blocks = {} -- table of blocks made from imported characters
+	local lineStart = 1 -- current first row for character insertion
 	for n,line in ipairs(text) do
-		table.insert(blocks, {})
+		local newLine = true -- var for knowing if there's a new 'line' (set of 5 tables by default) to be filled
 		for char in line:gmatch"." do
-			-- letter wrap
-			if lineBlocks > base.map.columns then
-				table.insert(blocks, {})
-				currLine = currLine + 1
-				lineBlocks = 0
+			-- #charBlock is 5
+			local charBlock = char2block(string.lower(char))
+			-- if we're going into a new line
+			if newLine then
+				-- if the previous line can hold no more chars, fill the tables with false
+				if lineStart ~= 1 then
+					for cnt=1,6 do
+						local num = lineStart - cnt
+						blocks[num] = tableFillFalse(blocks[num])
+					end
+				end
+				-- create new lines for new line
+				for cnt=0,5 do
+					table.insert(blocks,{})
+				end
+				newLine = false
 			end
-			charBlocks = char2block(string.lower(char))
-			-- + 1 because of empty space behind each char
-			lineBlocks = lineBlocks + #charBlocks[1] + 1
-			table.insert(blocks[currLine], charBlocks)
-		end
-		currLine = currLine + 1
-		lineBlocks = 0
+			-- insert the blocks
+			for o,crow in ipairs(charBlock) do
+				-- for each cell in a row
+				-- adding lines to blocks on need
+				for p,ccell in ipairs(crow) do
+					table.insert(blocks[lineStart - 1 + o], ccell)
+					-- add space unless end of row
+					if p == #charBlock[1] and #blocks[lineStart - 1 + o] < base.map.maxwidth then
+						table.insert(blocks[lineStart - 1 + o], false)
+					end
+				end --ccells
+			end --crow
+			-- check if
+			if #blocks[lineStart] + #charBlock[1] > base.map.maxwidth then
+				lineStart = lineStart + 6
+				newLine = true
+			end
+		end --char
+		newLine = true
+	end --line
+	-- following code is dirty solution to last line not having full width
+	lineStart = lineStart + 6
+	for cnt=1,6 do
+		local num = lineStart - cnt
+		blocks[num] = tableFillFalse(blocks[num])
 	end
 	return blocks
+end
+
+function tableFillFalse(tableFalse)
+	while #tableFalse < base.map.maxwidth do
+		table.insert(tableFalse,false)
+	end
+	return tableFalse
 end
 
 -- character goes in, representing block goes out
@@ -125,15 +173,16 @@ function love.update(dt)
 	if stage == "game" and timer.fall.counter >= timer.fall.limit then
 		timer.fall.counter = timer.fall.counter%timer.fall.limit
 		-- move attacker blocks if they're not high enough
-		if blocksAttacker.linepos > base.map.rows - 8 then
+		if blocksAttacker.linepos > base.map.maxheight - 8 then
 			blocksAttacker:move()
 		end
-		-- put falling blocks in fixed blocks if they are too high
-		if blocksFalling.collide(blocksFixed) then
-			
+		-- put falling blocks in fixed blocks if they are too high (collide with border or fixed blocks
+		if blocksFalling.collide(blocksFixed) or blocksFalling.linepos == 1 then
+			table.insert(blocksFixed,blocksFalling.blocks[1])
+			table.remove(blocksFalling.blocks,1)
 		-- move blocks if they're not too hight
 		else
-			
+			blocksFalling:move()
 		end
 	end
 end
@@ -144,25 +193,39 @@ function love.draw(dt)
 		
 	end
 	if stage == "game" then
-		pos = {x=base.block.size*2,y=blocksAttacker.linepos * base.block.size + base.block.size} 
-		--pos = {x=base.block.size*2,y=love.graphics.getHeight() - base.block.size*7}
-		for i,line in ipairs(blocksAttacker.blocks) do
-			for j,char in ipairs(line) do
-				for k,blockline in ipairs(char) do
-					for l,block in ipairs(blockline) do
-						if block then love.graphics.rectangle("fill",pos.x,pos.y,base.block.size,base.block.size) end
-						pos.x = pos.x + base.block.size --l%#blockline
-						love.graphics.setColor(math.random(100,255),math.random(100,255),math.random(100,255))
-					end -- block
-					pos.x = pos.x - #blockline*base.block.size
-					pos.y = pos.y + base.block.size
-				end -- blockline
-			pos.x = pos.x + (#char[1] + 1)*base.block.size
-			pos.y = pos.y - base.block.size*5
-			end -- char
-		pos.x = base.block.size*2
-		pos.y = pos.y + base.block.size*6
-		end -- line
+		-- attacker blocks
+		local pos = {
+			x = base.block.size * 2,
+			y = blocksAttacker.linepos * base.block.size + base.block.size,
+		} 
+		for i,bline in ipairs(blocksAttacker.blocks) do
+			for j,bcell in ipairs(bline) do
+				colorRandomize("mid")
+				if bcell then love.graphics.rectangle("fill", pos.x, pos.y, base.block.size, base.block.size) end
+				colorRandomize("dark")
+				if not bcell then love.graphics.rectangle("fill", pos.x, pos.y, base.block.size, base.block.size) end
+				pos.x = pos.x + base.block.size
+			end -- bcell
+			pos.x = base.block.size * 2
+			pos.y = pos.y + base.block.size
+		end -- bline
+		-- attacker blocks
+		local pos = {
+			x = base.block.size * 2,
+			y = blocksFalling.linepos * base.block.size + base.block.size,
+		} 
+		-- falling blocks
+		for i,bline in ipairs(blocksFalling.blocks) do
+			for j,bcell in ipairs(bline) do
+				colorRandomize("light")
+				if bcell then love.graphics.rectangle("fill", pos.x, pos.y, base.block.size, base.block.size) end
+				colorRandomize("dark")
+				if not bcell then love.graphics.rectangle("fill", pos.x, pos.y, base.block.size, base.block.size) end
+				pos.x = pos.x + base.block.size
+			end -- bcell
+			pos.x = base.block.size * 2
+			pos.y = pos.y + base.block.size
+		end -- bline
 	end -- if stage is game
 end
 
@@ -171,13 +234,27 @@ function love.keypressed(key, unicode)
 	if key == '' or key == 'escape' then
 		love.event.push('q') -- quit the game 
 	end
-	if stage == "title" then
+	if stage == "game" then
+		if key == "right" or "left" then
+			blocksFalling:sidestep(key)
+		end
+	elseif stage == "title" then
 		if key == 'up' or key == 'left' or key == 'right' then
 			keyPressed[key] = true
 			if keyPressed.up and keyPressed.left and keyPressed.right then
 				startWar("test.txt")
 			end
 		end
+	end
+end
+
+function colorRandomize(tone)
+	if tone == "light" then
+		love.graphics.setColor(math.random(200,255),math.random(200,255),math.random(200,255))
+	elseif tone == "mid" then
+		love.graphics.setColor(math.random(100,255),math.random(100,255),math.random(100,255))
+	elseif tone == "dark" then
+		love.graphics.setColor(math.random(50,55),math.random(50,55),math.random(50,55))
 	end
 end
 
